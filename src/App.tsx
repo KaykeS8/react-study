bde7f05c3af185323d8109677152e17cdd8c8a60
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-07-03 20:36:53 -0300

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import React, { useState } from 'react';
-import { Produto } from './Produto';
+import React, { useEffect, useRef } from "react";
 
 const App = () => {
+  const [carrinho, setCarrinho] = React.useState(0);
+  const [notification, setNotification] = React.useState<string>("");
+
+  const timeOutReference = React.useRef(0)
+
+  function handleClick() {
+    setCarrinho(carrinho + 1);
+    setNotification("Item adicionado ao carrinho")
+
+    clearTimeout(timeOutReference.current)
+    timeOutReference.current = setTimeout(() => {
+      setNotification("")
+    }, 2000);
+    console.log(timeOutReference.current)
+  }
+
   return (
     <div className="App">
-      <Produto />
+      <p>{notification}</p>
+      <button onClick={handleClick}>Adicionar ao carrinho {carrinho}</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
