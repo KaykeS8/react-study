334f61d04089638206501924b7449844c8abf76c
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-07-03 11:46:02 -0300

    Componente APP

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { Produto } from './Produto';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Produto />
+    </div>
+  )
+}
+
+export default App
+
