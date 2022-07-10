23343a09ceae1b7275b0b042bcb1dbcbae4fae9b
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-07-10 11:35:04 -0300

    Aula de useRef criar referência para elementos no React

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React, { useEffect, useRef } from "react";
+import { ComputerTower } from "phosphor-react";
+import React, { useEffect, useRef, useState } from "react";
 
 const App = () => {
-  const [carrinho, setCarrinho] = React.useState(0);
-  const [notification, setNotification] = React.useState<string>("");
 
-  const timeOutReference = React.useRef(0)
+  const [carrinho, setCarrinho] = useState(0)
+  const [notification, setNotification] = useState<string | undefined>(undefined)
+
+  const notificationRef = useRef<number>()
 
   function handleClick() {
-    setCarrinho(carrinho + 1);
-    setNotification("Item adicionado ao carrinho")
-
-    clearTimeout(timeOutReference.current)
-    timeOutReference.current = setTimeout(() => {
-      setNotification("")
-    }, 2000);
-    console.log(timeOutReference.current)
+    setCarrinho(carrinho + 1)
+    setNotification('Item adicionado ao carrinho')
+    clearInterval(notificationRef.current)
+    notificationRef.current = setTimeout(() => {
+      setNotification(undefined)
+    }, 2000)
+    console.log(notificationRef.current)
   }
 
   return (
     <div className="App">
       <p>{notification}</p>
-      <button onClick={handleClick}>Adicionar ao carrinho {carrinho}</button>
+      <button onClick={handleClick}>Adicionar carrinho</button>
+      {carrinho ? <p>Total de Produtos:{carrinho}</p> : undefined}
     </div>
   );
 };
