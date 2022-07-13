c39cc111d999dc313a9c0189913624d70923dae7
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-07-13 20:59:07 -0300

    Componente Produto para comsumir o contexto global

--- src/Produto.tsx
+++ src/Produto.tsx
@@ -1,48 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import { GlobalContext } from './GlobalContext';
 
-interface Idados {
-  nome?: string
-  preco?: string
-}
 
 export const Produto = () => {
-  const [dados, setDados] = React.useState<Idados>({})
-  const [preference, setPreference] = React.useState<any>(null)
 
-  async function handleFetch(event: any) {
-    setPreference(event.target.innerText)
-    const response = await fetch(`https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`)
-    const dadosJson = await response.json()
-    setDados(dadosJson)
-  }
-
-  useEffect(() => {
-    if (preference !== null) {
-      localStorage.setItem("Produto", preference)
-    }
-  }, [preference])
-
-  useEffect(() => {
-    const productPreference = window.localStorage.getItem("Produto")
-    fetch(`https://ranekapi.origamid.dev/json/api/produto/${productPreference}`)
-      .then(response => response.json())
-      .then(json => setDados(json))
-  }, [])
-
+  const dados = React.useContext(GlobalContext)
+  const { count, addCountOne } = dados
 
   return (
     <div>
-      <h1>Preferencia:{dados.nome}</h1>
-      <button onClick={handleFetch}>notebook</button>
-      <button onClick={handleFetch}>smartphone</button>
-      {
-        dados && (
-          <>
-            <h2>{dados.nome}</h2>
-            <p>{dados.preco}</p>
-          </>
-        )
-      }
-    </div >
+      <button onClick={() => addCountOne()}>Adicionar</button>
+      <br />
+      Prouduto:{count}
+      <br />
+      status: Em estoque
+    </div>
   )
 }
