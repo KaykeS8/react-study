4af2e3e39e12a93fe184541396a1876ea0a88045
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-07-16 13:17:44 -0300

    CustoHooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,47 @@
-import React from 'react';
-import { useLocalStorage } from './useLovalStorage';
+import React from "react";
+import { useLocalStorage } from "./useLovalStorage";
+import { useFetch } from "./useFetch";
+
+type Tproduto = {
+  nome: string;
+  id: string;
+};
 
 const App = () => {
+  const [produto, setProduto] = useLocalStorage("Produto", "");
+  const { request, data, loading, error } = useFetch();
 
-  const [produto, setProduto] = useLocalStorage('Produto', '')
+  React.useEffect(() => {
+    async function fetchData() {
+      const { json, response } = await request("https://ranekapi.origamid.dev/json/api/produto/")
+    }
+    fetchData()
+  }, []);
 
-  const handleClick = ({ currentTarget }: React.MouseEvent<HTMLButtonElement>) => {
-    setProduto(currentTarget.innerText)
-  }
 
-  return (
-    <div>
-      <h2>Produto preferido: {produto}</h2>
-      <button onClick={handleClick}>Notebook</button>
-      <button onClick={handleClick}>smartphone</button>
-    </div>
-  )
-}
+  const handleClick = ({
+    currentTarget,
+  }: React.MouseEvent<HTMLButtonElement>) => {
+    setProduto(currentTarget.innerText);
+  };
 
-export default App
+
+  if (error) return <p>{error}</p>
+  if (loading) return <p>...Carregando</p>
+  if (data)
+    return (
+      <div>
+        <h2>Produto preferido: {produto}</h2>
+        <button onClick={handleClick}>notebook</button>
+        <button onClick={handleClick}>smartphone</button>
+        {data.map((produto: Tproduto) => (
+          <div key={produto.id}>
+            <h1>{produto.nome}</h1>
+          </div>
+        ))}
+      </div>
+    );
+  else return null
+};
+
+export default App;
