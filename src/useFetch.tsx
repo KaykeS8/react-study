8f3effc0d8469f4fe92f0e678b811240cc31ee79
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-07-18 13:29:13 -0300

    custom Hook para fazer fetch

--- src/useFetch.tsx
+++ src/useFetch.tsx
@@ -1,29 +1,28 @@
-import React from 'react'
+import React from "react";
 
 export const useFetch = () => {
-  const [data, setData] = React.useState<any>([])
-  const [error, setError] = React.useState<string | null>(null)
-  const [loading, setLoading] = React.useState<boolean>(false)
+  const [data, setData] = React.useState(null);
+  const [error, setError] = React.useState<string | null>(null);
+  const [loading, setLoading] = React.useState<boolean>(false);
 
-  const request = React.useCallback(async function <T>(url: string, options?: T) {
+  async function request<T>(url: string, options?: T) {
     let response;
     let json;
     try {
-      setError(null)
-      setLoading(true)
-      response = await fetch(url, options)
-      json = await response.json()
-      setLoading(false)
-    } catch (error: any) {
-      json = null
-      setError('Ocorreu um erro na aplicação')
-      console.error(error)
+      setLoading(true);
+      response = await fetch(url, options);
+      json = await response.json();
+      setLoading(false);
+    } catch (error) {
+      setError("Ocorreu um erro");
+      json = null;
+      console.error(error);
     } finally {
-      setData(json)
-      setLoading(false)
-      return { response, json }
+      setLoading(false);
+      setData(json);
+      return { response, json };
     }
-  }, [])
+  }
 
-  return { data, error, loading, request }
-}
+  return { data, error, loading, request };
+};
