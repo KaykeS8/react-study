0607a9ce147a1753f101b40d08d2071726e1f486
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-07-19 12:35:31 -0300

    Formularios no REACT

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,49 @@
 import React from "react";
+import { X } from "phosphor-react";
 
 const App = () => {
-  const [radio, setRadio] = React.useState("");
+  const [text, setText] = React.useState<string>("");
+  const [message, setMessage] = React.useState<string[]>([]);
+  const inputRef = React.useRef<HTMLInputElement>(null);
 
   function handleChange({ target }: React.ChangeEvent<HTMLInputElement>) {
-    setRadio(target.value);
+    setText(target.value);
+  }
+
+  function addMessage() {
+    setMessage(() => {
+      return text.length > 0 ? [...message, text] : [...message];
+    });
+    setText("");
+    inputRef.current?.focus();
+  }
+
+  function removeMessage() {
+    console.log("removeu");
+    setMessage(message.filter((item,index,array) => {
+      return array[index] !== message[index]
+    }));
   }
 
   return (
-    <form>
-      <label>
-        <input
-          type="radio"
-          name="sexo"
-          id="sexo"
-          value="masculino"
-          onChange={handleChange}
-        />
-        Masculino
-      </label>
-      <label>
-        <input
-          type="radio"
-          name="sexo"
-          id="sexo"
-          value="femenino"
-          onChange={handleChange}
-        />
-        femenino
-      </label>
-      <p>item selecionado {radio}</p>
+    <form onSubmit={(e) => e.preventDefault()}>
+      <input
+        type="text"
+        ref={inputRef}
+        name="texto"
+        id="texto"
+        placeholder="digite sua messagem"
+        value={text}
+        onChange={handleChange}
+      />
+      <button onClick={addMessage}>Adicionar tarefa</button>
+      <h2>Tarefas</h2>
+      {message.map((item, index) => (
+        <li key={index} style={{ display: "flex", alignItems: "center" }}>
+          {item}
+          <X style={{ cursor: "pointer" }} onClick={removeMessage} />
+        </li>
+      ))}
     </form>
   );
 };
