bc33ace6036b5a616de63fe08b27d6706440540a
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-07-21 11:00:58 -0300

    Componente Select

---
+++ src/Form/Selects.tsx
@@ -0,0 +1,24 @@
+import React, { Children } from 'react';
+
+
+interface PropsSelect {
+  options: string[];
+  value: string;
+  setValue: (item: string) => void
+}
+
+export const Select = ({ options, setValue, value, ...props }: PropsSelect) => {
+
+  function handleChange({ target }: React.ChangeEvent<HTMLSelectElement>) {
+    setValue(target.value)
+  }
+
+  return (
+    <select value={value} onChange={handleChange} {...props}>
+      <option value='' disabled>Produtos</option>
+      {options.map(opt => (
+        <option key={opt} value={opt} style={{ textTransform: "capitalize" }}>{opt}</option>
+      ))}
+    </select>
+  )
+}
