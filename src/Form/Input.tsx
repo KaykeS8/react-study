0969595ffe18757015215424062cd6c742709af5
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-07-28 10:53:08 -0300

    Componente de input

--- src/Form/Input.tsx
+++ src/Form/Input.tsx
@@ -3,20 +3,22 @@ import React from 'react';
 interface PropsInput {
   type: string;
   value: string;
-  onChange: ({target}:React.ChangeEvent<HTMLInputElement>) => void;
+  onChange: ({ target }: React.ChangeEvent<HTMLInputElement>) => void;
   label: string;
   id: string;
   placeholder?: any
-  onBlur: any;
+  onBlur: () => boolean;
+  error: string | null
 }
 
 
-export const Input = ({ id, label, onChange, type, value, ...props }: PropsInput) => {
+export const Input = ({ id, label, onChange, onBlur, type, value, placeholder, error }: PropsInput) => {
 
   return (
     <div>
       <label htmlFor={id}>{label}</label>
-      <input type={type} id={id} name={id} value={value} onChange={onChange} {...props} />
+      <input type={type} id={id} name={id} value={value} onChange={onChange} onBlur={onBlur} placeholder={placeholder} />
+      {error && <p>{error}</p>}
     </div>
   )
 }
