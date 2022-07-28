d41914fe029190b4cef4159b19e3b963f425ddb4
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-07-28 10:52:43 -0300

    Arquivo App

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,29 @@ import { useForm } from './Hooks/useForm';
 
 const App = () => {
 
-  const cep = useForm('cep')
+  const cep = useForm('cep');
+  const email = useForm('email');
+  const nome = useForm();
 
   function handleSubmit(event: React.FormEvent) {
     event.preventDefault();
-    console.log(cep)
+    if (cep.validate() && email.validate() && nome.validate()) {
+      console.log({
+        cep: cep.value,
+        email: email.value,
+        nome: nome.value
+      })
+    } else {
+      console.log("Errorrrr")
+    }
   }
-
 
   return (
     <form onSubmit={handleSubmit}>
-      <Input id="cep" label="CEP" type="text" placeholder='00000-000' {...cep}/>
-      <button>Enviar</button>
+      <Input id="nome" label="Nome" type="text" placeholder='Digite seu nome' {...nome} />
+      <Input id="cep" label="CEP" type="text" placeholder='00000-000' {...cep} />
+      <Input id="cep" label="Email" type="email" placeholder='Digite seu E-mail' {...email} />
+      {email.error || cep.error || nome.error ? <button disabled>Enviar</button> : <button>Enviar</button>}
     </form>
   );
 };
