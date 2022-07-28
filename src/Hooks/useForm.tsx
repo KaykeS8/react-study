33ab993e6219696de956d6fe24240d500cafbdc1
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-07-28 10:54:33 -0300

    hook para formularios responsável  por controlar o estado dos inputs e validação dos dados

--- src/Hooks/useForm.tsx
+++ src/Hooks/useForm.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 
-type typeOptions = 'cep' | 'email'
+type typeOptions = 'cep' | 'email' | 'nome';
 type tipagemTypes = {
   cep: {
+    regex: RegExp,
+    message: string
+  },
+  email: {
     regex: RegExp,
     message: string
   }
@@ -13,10 +17,14 @@ const types: tipagemTypes = {
   cep: {
     regex: /^\d{5}-?\d{3}$/,
     message: 'Cep inválido'
+  },
+  email: {
+    regex: /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
+    message: 'Digite um email válido'
   }
 }
 
-export const useForm = (type: typeOptions) => {
+export const useForm = (type?: typeOptions) => {
   const [value, setValue] = React.useState("");
   const [error, setError] = React.useState<string | null>(null);
 
@@ -24,7 +32,7 @@ export const useForm = (type: typeOptions) => {
     if (value.length === 0) {
       setError("Preencha um valor")
       return false
-    } else if (!types[type as keyof tipagemTypes].regex.test(value)) {
+    } else if (types[type as keyof tipagemTypes] && !types[type as keyof tipagemTypes].regex.test(value)) {
       setError(types[type as keyof tipagemTypes].message)
       return false
     } else {
@@ -43,6 +51,7 @@ export const useForm = (type: typeOptions) => {
     setValue,
     onChange,
     error,
-    onBlur: () => validate(value)
+    onBlur: () => validate(value),
+    validate: () => validate(value)
   }
 }
