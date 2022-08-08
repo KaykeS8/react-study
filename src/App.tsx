b0e3bfe4a41261479cd4f72290ac6787fff7c419
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-08-08 17:43:56 -0300

    Criação de um slide

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React from "react";
-import { Header } from "./Header";
+import { Slide } from "./Slide";
 
 const App = () => {
+  const Slides = [
+    {
+      id: "slide1",
+      text: "Slide1",
+    },
+    {
+      id: "slide2",
+      text: "Slide2",
+    },
+    {
+      id: "slide3",
+      text: "Slide3",
+    },
+  ];
 
   return (
-    <div> 
-      <Header/>
+    <div>
+      <Slide slides={Slides} />
     </div>
   );
 };
