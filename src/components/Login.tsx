4bfdd75cc1337c14c12c14ae66d2b164cd4aaabb
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-08-11 23:31:35 -0300

    Página de Login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,17 +1,85 @@
-import { Box, Button, Heading } from '@chakra-ui/react'
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Stack,
+  Link,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
 
-export const Login = () => {
+export function Login() {
 
   const navigate = useNavigate();
-  function handleClick() {
-    navigate('/sobr')
+  const [email, setEmail] = React.useState<string | undefined>(undefined);
+  const [password, setPassword] = React.useState<string | undefined>(undefined)
+
+  function handleChange({target}:React.ChangeEvent<HTMLInputElement>) {
+    setEmail(target.value)
+    setPassword(target.value)
   }
+
+
+  function handleClick() {
+    navigate('/kjh')
+  }
+
   return (
-    <Box m={3}>
-      <Heading>Login</Heading>
-      <Button onClick={handleClick}>Login</Button>
-    </Box>
-  )
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      bg={useColorModeValue('gray.50', 'gray.800')}>
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'4xl'}>Sign in to your account</Heading>
+          <Text fontSize={'lg'} color={'gray.600'}>
+            to enjoy all of our cool <Link color={'blue.400'}>features</Link> ✌️
+          </Text>
+        </Stack>
+        <Box
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'lg'}
+          p={8}>
+          <Stack spacing={4}>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" value={email} onChange={handleChange}/>
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <Input type="password"/>
+            </FormControl>
+            <Stack spacing={10}>
+              <Stack
+                direction={{ base: 'column', sm: 'row' }}
+                align={'start'}
+                justify={'space-between'}>
+                <Checkbox>Remember me</Checkbox>
+                <Link color={'blue.400'}>Forgot password?</Link>
+              </Stack>
+              <Button
+                bg={'blue.400'}
+                color={'white'}
+                _hover={{
+                  bg: 'blue.500',
+                }}
+                  onClick={handleClick}
+                >
+                Sign in
+              </Button>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
 }
