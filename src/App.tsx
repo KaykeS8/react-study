509b6011eb6ea61d2c9c0a4dce29159623f7859c
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-08-19 16:09:34 -0300

    Componente APP com todas as rotas do aplicativo

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import { Home } from "./components/Home";
 import { Login } from "./components/Login";
 import { NaoEcontrado } from "./components/NaoEcontrado";
 import { Sobre } from "./components/Sobre";
+import { Produto } from "./components/Produtos/Produto";
+import { ProdutoAvaliacao } from "./components/ProdutoAvaliacao";
+import { ProdutoCustomizado } from "./components/ProdutoCustomizado";
+import { ProdutoDescricao } from "./components/ProdutoDescricao";
 
 const App = () => {
   return (
@@ -13,8 +17,13 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="sobre" element={<Sobre />} />
-        <Route path="login" element={<Login/>}/>
+        <Route path="login" element={<Login />} />
         <Route path="*" element={<NaoEcontrado />} />
+        <Route path="produto/:id/*" element={<Produto />}>
+          <Route path="" element={<ProdutoDescricao />} />
+          <Route path="avaliacao" element={<ProdutoAvaliacao />} />
+          <Route path="customizado" element={<ProdutoCustomizado />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,84 +1,39 @@
-import React from 'react';
+import React from "react";
 import {
   Flex,
-  Box,
   FormControl,
   FormLabel,
   Input,
-  Checkbox,
   Stack,
-  Link,
   Button,
   Heading,
-  Text,
   useColorModeValue,
-} from '@chakra-ui/react';
-import { useNavigate } from 'react-router-dom';
+} from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 export function Login() {
-
   const navigate = useNavigate();
-  const [email, setEmail] = React.useState<string | undefined>(undefined);
-  const [password, setPassword] = React.useState<string | undefined>(undefined)
-
-  function handleChange({target}:React.ChangeEvent<HTMLInputElement>) {
-    setEmail(target.value)
-    setPassword(target.value)
-  }
-
-
   function handleClick() {
-    navigate('/kjh')
+    navigate("/kjh");
   }
 
   return (
-    <Flex
-      minH={'100vh'}
-      align={'center'}
-      justify={'center'}
-      bg={useColorModeValue('gray.50', 'gray.800')}>
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'4xl'}>Sign in to your account</Heading>
-          <Text fontSize={'lg'} color={'gray.600'}>
-            to enjoy all of our cool <Link color={'blue.400'}>features</Link> ✌️
-          </Text>
-        </Stack>
-        <Box
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'lg'}
-          p={8}>
-          <Stack spacing={4}>
-            <FormControl id="email">
-              <FormLabel>Email address</FormLabel>
-              <Input type="email" value={email} onChange={handleChange}/>
-            </FormControl>
-            <FormControl id="password">
-              <FormLabel>Password</FormLabel>
-              <Input type="password"/>
-            </FormControl>
-            <Stack spacing={10}>
-              <Stack
-                direction={{ base: 'column', sm: 'row' }}
-                align={'start'}
-                justify={'space-between'}>
-                <Checkbox>Remember me</Checkbox>
-                <Link color={'blue.400'}>Forgot password?</Link>
-              </Stack>
-              <Button
-                bg={'blue.400'}
-                color={'white'}
-                _hover={{
-                  bg: 'blue.500',
-                }}
-                  onClick={handleClick}
-                >
-                Sign in
-              </Button>
-            </Stack>
-          </Stack>
-        </Box>
+    <Flex minH={"100vh"} align={"center"} justify={"center"}>
+      <Stack bg={useColorModeValue("white", "gray.700")} padding={5} borderRadius={'md'} boxShadow={'xl'}>
+        <Heading>Create Account</Heading>
+        <FormControl>
+          <FormLabel>Email</FormLabel>
+          <Input type={"email"}/>
+        </FormControl>
+        <FormControl>
+          <FormLabel>Password</FormLabel>
+          <Input type={"password"} />
+        </FormControl>
+        <Button colorScheme={'blue'}
+          onClick={handleClick}
+        >
+          Enviar
+        </Button>
       </Stack>
     </Flex>
   );
