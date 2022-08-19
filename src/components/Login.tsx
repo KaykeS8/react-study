620b9c8fff27bd0eaf4a92468fe5cfde25cc159c
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-08-19 16:12:39 -0300

    Página de login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -13,25 +13,29 @@ import { useNavigate } from "react-router-dom";
 
 export function Login() {
   const navigate = useNavigate();
+
   function handleClick() {
     navigate("/kjh");
   }
 
   return (
     <Flex minH={"100vh"} align={"center"} justify={"center"}>
-      <Stack bg={useColorModeValue("white", "gray.700")} padding={5} borderRadius={'md'} boxShadow={'xl'}>
+      <Stack
+        bg={useColorModeValue("white", "gray.700")}
+        padding={5}
+        borderRadius={"md"}
+        boxShadow={"xl"}
+      >
         <Heading>Create Account</Heading>
         <FormControl>
           <FormLabel>Email</FormLabel>
-          <Input type={"email"}/>
+          <Input type={"email"} />
         </FormControl>
         <FormControl>
           <FormLabel>Password</FormLabel>
           <Input type={"password"} />
         </FormControl>
-        <Button colorScheme={'blue'}
-          onClick={handleClick}
-        >
+        <Button colorScheme={"blue"} onClick={handleClick}>
           Enviar
         </Button>
       </Stack>
