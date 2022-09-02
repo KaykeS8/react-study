04849ed98935c88798413e43b59546af65c3d609
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-09-02 13:42:03 -0300

    Página de notFound

---
+++ src/pages/PageNotFound.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  Box,
+  Center,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from "@chakra-ui/react";
+import { Head } from "../Components/Head";
+
+export const PageNotFound = () => {
+  return (
+    <Box>
+      <Head title="error"/>
+      <Center height={"50vh"}>
+        <Alert status="error" flexDirection={'column'} w={'auto'} p={5} borderRadius={"md"} boxShadow={'lg'}>
+          <AlertIcon />
+          <AlertTitle>Page Not Found</AlertTitle>
+          <AlertDescription>Error-404 The page you requested could not be found</AlertDescription>
+        </Alert>
+      </Center>
+    </Box>
+  );
+};
