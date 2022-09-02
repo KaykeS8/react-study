02f660aa0101d68f9e5b3c5d020e340f4a06595b
Kayke simão <89744379+KaykeS8@users.noreply.github.com>
2022-09-02 13:44:44 -0300

    Okay

--- src/components/Produtos/ProdutoAvaliacao.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const ProdutoAvaliacao = () => {
-  return (
-    <div>
-      <h2>Produto Avaliação</h2>
-    </div>
-  )
-}
--- src/components/Sobre.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Sobre = () => {
-  return (
-    <div>
-      Página sobre
-    </div>
-  )
-}
